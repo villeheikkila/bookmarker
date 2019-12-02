97c9a2693ad45d13fc989b20583c04cc3f5423ca
Yusif Salam-zade <yusalam@gmail.com>
2019-12-02 14:08:59 +0200

    implement sorting to table of all items

--- src/components/ItemsTable.js
+++ src/components/ItemsTable.js
@@ -1,26 +1,64 @@
-import React from 'react'
+import _ from 'lodash'
+import React, { useEffect, useState } from 'react'
 import { Table } from 'semantic-ui-react'
 
 export const ItemsTable = ({ items }) => {
+
+    const [data, setData] = useState({
+        column: "",
+        data: "",
+        direction: "",
+    })
+
+    useEffect(() => {
+        if (items.length !== 0) {
+            setData(prevState => ({
+                ...prevState,
+                data: Object.values(items).reduce((result, prev) => result.concat(prev.map(p => ({ type: p.type, author: p.author, title: p.author }))), [])
+            }))
+        }
+    }, [items])
+
+    const handleSort = (clickedColumn, st) => () => {
+        const { column, data, direction } = st
+        if (column !== clickedColumn) {
+            setData({
+                column: clickedColumn,
+                data: _.sortBy(st.data, clickedColumn),
+                direction: 'ascending',
+            }
+            )
+            return
+        }
+        setData(prevState => ({
+            ...prevState,
+            data: data.reverse(),
+            direction: direction === 'ascending' ? 'descending' : 'ascending',
+        }))
+
+    }
     return (
         <div>
-            <Table>
+            <Table sortable celled>
                 <Table.Header>
                     <Table.Row>
-                        <Table.HeaderCell>Type</Table.HeaderCell>
-                        <Table.HeaderCell>Author</Table.HeaderCell>
-                        <Table.HeaderCell>Title</Table.HeaderCell>
+                        <Table.HeaderCell sorted={data.column === 'type' ? data.direction : null}
+                            onClick={handleSort('type', data)}>Type</Table.HeaderCell>
+                        <Table.HeaderCell sorted={data.column === 'author' ? data.direction : null}
+                            onClick={handleSort('author', data)}>Author</Table.HeaderCell>
+                        <Table.HeaderCell sorted={data.column === 'title' ? data.direction : null}
+                            onClick={handleSort('title', data)}>Title</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
 
                 <Table.Body>
-                    {items.length !== 0 ? Object.keys(items).map(arr => items[arr].length !== 0 ? items[arr].map(item => 
-                        <Table.Row key={item.id}> 
+                    {data.data.length !== 0 ? data.data.map(item =>
+                        <Table.Row key={item.id}>
                             <Table.Cell>{item.type}</Table.Cell>
                             <Table.Cell>{item.author}</Table.Cell>
                             <Table.Cell>{item.title}</Table.Cell>
                         </Table.Row>
-                    ):null): <Table.Row ><Table.Cell>loading...</Table.Cell></Table.Row>}
+                    ) : <Table.Row ><Table.Cell>loading...</Table.Cell></Table.Row>}
 
                 </Table.Body>
             </Table>
