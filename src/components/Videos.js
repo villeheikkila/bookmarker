10fee17d2cd7be3ad400369c5e7896f6c0036b31
Yusif Salam-zade <yusalam@gmail.com>
2019-12-02 15:29:35 +0200

    video tab is sortable

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,10 +1,47 @@
-import React from 'react'
+import _ from 'lodash'
+import React, { useEffect, useState } from 'react'
+import { Button } from 'semantic-ui-react'
 import { Video } from './Video'
 
 export const Videos = ({ videos }) => {
+    const [sortedVideos, setSortedVideos] = useState([])
+    const [sortDirections, setSortDirections] = useState({
+        author: true,
+        title: true,
+    })
+
+    useEffect(() => {
+        if (videos.length !== 0) {
+            setSortedVideos(videos)
+        }
+    }, [videos])
+
+    const sortByAuthor = () => {
+        const clone = sortDirections.author ? _.sortBy(sortedVideos, ['author']) : _.sortBy(sortedVideos, ['author']).reverse()
+        setSortedVideos(clone)
+        setSortDirections(prev => ({
+            ...prev,
+            author: !prev.author
+        }))
+    }
+
+    const sortByTitle = () => {
+        const clone = sortDirections.author ? _.sortBy(sortedVideos, ['title']) : _.sortBy(sortedVideos, ['title']).reverse()
+        setSortedVideos(clone)
+        setSortDirections(prev => ({
+            ...prev,
+            title: !prev.title
+        }))
+    }
+
     return (
         <div>
-            {videos.map(v => <Video key={v.id} video={v} />)}
+            <Button.Group basic size='large' style={{display:'flex', marginBottom: '10px'}}>
+                <Button onClick={sortByAuthor}>Sort by author</Button>
+                <Button onClick={sortByTitle}>Sort by title</Button>
+            </Button.Group>
+
+            {sortedVideos.map(v => <Video key={v.id} video={v} />)}
         </div>
     )
 } 
