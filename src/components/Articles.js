6ae39156964b5a58fcc4d5fd356659d564f52d70
Yusif Salam-zade <yusalam@gmail.com>
2019-12-07 14:38:29 +0200

    articles can be sorted

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,8 +1,16 @@
+import _ from 'lodash'
 import React, { useEffect, useState } from 'react'
+import { Button } from 'semantic-ui-react'
 import { Article } from './Article'
 
 export const Articles = ({ articles }) => {
     const [sortedArticles, setSortedArticles] = useState([])
+    const [sortDirections, setSortDirections] = useState({
+        author: true,
+        title: true,
+        publisher: true,
+        date: true
+    })
 
     useEffect(() => {
         if (articles !== undefined && articles.length !== 0) {
@@ -11,8 +19,54 @@ export const Articles = ({ articles }) => {
         else setSortedArticles([])
     }, [articles])
 
+    const sortByAuthor = () => {
+        const clone = sortDirections.author ? _.sortBy(sortedArticles, ['author']) : _.sortBy(sortedArticles, ['author']).reverse()
+        setSortedArticles(clone)
+        setSortDirections(prev => ({
+            ...prev,
+            author: !prev.author
+        }))
+    }
+
+    const sortByTitle = () => {
+        const clone = sortDirections.title ? _.sortBy(sortedArticles, ['title']) : _.sortBy(sortedArticles, ['title']).reverse()
+        setSortedArticles(clone)
+        setSortDirections(prev => ({
+            ...prev,
+            title: !prev.title
+        }))
+    }
+
+    const sortByPublisher = () => {
+        const clone = sortDirections.publisher ? _.sortBy(sortedArticles, ['publisher']) : _.sortBy(sortedArticles, ['publisher']).reverse()
+        setSortedArticles(clone)
+        setSortDirections(prev => ({
+            ...prev,
+            publisher: !prev.publisher
+        }))
+    }
+
+    const sortByDate = () => {
+        const clone = sortDirections.date ? _.sortBy(sortedArticles, function (article) {
+            return new Date(article.date)
+        }) : _.sortBy(sortedArticles, function (article) {
+            return new Date(article.date)
+        }).reverse()
+        setSortedArticles(clone)
+        setSortDirections(prev => ({
+            ...prev,
+            date: !prev.date
+        }))
+    }
+
     return (
         <div>
+            <Button.Group basic size='large' color='green' inverted style={{display:'flex', marginBottom: '10px'}}>
+                <Button onClick={sortByAuthor}>Sort by author</Button>
+                <Button onClick={sortByTitle}>Sort by title</Button>
+                <Button onClick={sortByPublisher}>Sort by publisher</Button>
+                <Button onClick={sortByDate}>Sort by date</Button>
+            </Button.Group>
             {sortedArticles.map(a => <Article key={a.id} article={a} />)}
         </div>
     )
