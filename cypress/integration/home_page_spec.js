a659638e347f2ea688115f589e6d8f9117d508bc
Ville Heikkilä <tehomail@gmail.com>
2019-12-19 15:57:57 +0200

    Fix remaining linting errors

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,5 +1,5 @@
 describe('The Home Page Loads', function() {
     it('successfully loads', function() {
-      cy.visit('http://localhost:3000')
-    })
-  })
+        cy.visit('http://localhost:3000');
+    });
+});
--- src/__tests__/Book-test.js
+++ src/__tests__/Book-test.js
@@ -7,24 +7,24 @@ import { Book } from '../components/Book';
 Enzyme.configure({ adapter: new Adapter() });
 
 const example = {
-  "id": 30,
-  "kirjoittaja": "Robert Martin",
-  "otsikko": "Clean Code: A Handbook of Agile Software Craftsmanship",
-  "isbn": "978-0132350884",
-  "tagit": ["Ohjelmointi, design patterns, agile"],
-  "related": ["TKT20006 Ohjelmistotuotanto"]
-}
+    id: 30,
+    kirjoittaja: 'Robert Martin',
+    otsikko: 'Clean Code: A Handbook of Agile Software Craftsmanship',
+    isbn: '978-0132350884',
+    tagit: ['Ohjelmointi, design patterns, agile'],
+    related: ['TKT20006 Ohjelmistotuotanto'],
+};
 
 describe('Book component', () => {
+    it('Renders all fields if given valid props', () => {
+        const wrapper = shallow(<Book book={example} />);
 
-  it('Renders all fields if given valid props', () => {
-
-    const wrapper = shallow(<Book book={example} />);
-
-    expect(wrapper.contains(<TableCell as="td">Robert Martin</TableCell>)).toBe(false);
-    expect(wrapper.contains(<TableCell as="td">Clean Code: A Handbook of Agile Software Craftsmanship</TableCell>)).toBe(false);
-    expect(wrapper.contains(<TableCell as="td">978-0132350884</TableCell>)).toBe(true);
-    // expect(wrapper.contains(<Label>Ohjelmointi, design patterns, agile</Label>)).toBe(true);
-    // expect(wrapper.contains(<Label>TKT20006 Ohjelmistotuotanto</Label>)).toBe(true);
-  });
+        expect(wrapper.contains(<TableCell as="td">Robert Martin</TableCell>)).toBe(false);
+        expect(
+            wrapper.contains(<TableCell as="td">Clean Code: A Handbook of Agile Software Craftsmanship</TableCell>),
+        ).toBe(false);
+        expect(wrapper.contains(<TableCell as="td">978-0132350884</TableCell>)).toBe(true);
+        // expect(wrapper.contains(<Label>Ohjelmointi, design patterns, agile</Label>)).toBe(true);
+        // expect(wrapper.contains(<Label>TKT20006 Ohjelmistotuotanto</Label>)).toBe(true);
+    });
 });
